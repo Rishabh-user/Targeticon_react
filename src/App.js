708f49113d1fcc4af66f59dfd14e3506933c2b92
saryusirohi30 <saryu@targeticon.com>
2023-07-20 12:07:17 +0530

    [Link and routning]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Header from "./components/header";
+import Footer from "./components/footer";
 import Home from './pages/Home/home';
 import About from './pages/About/about';
 import HireDevelopers from './pages/hire-app-developers/hire-app-developers';
 
 function App() {
   return (
-    <div>
-      <Router forceRefresh>
-        {/* <div>
-          <Link to="/"></Link>
-          <Link to="/about"></Link>
-          <Link to="/hire-developers"></Link>
-        </div> */}
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/hire-developers" element={<HireDevelopers />} />
-        </Routes>
-      </Router>
+    <div>   
+      <BrowserRouter>
+        <Header />        
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/hire-developers" element={<HireDevelopers />} />
+            </Routes>
+            <Footer />  
+      </BrowserRouter>
     </div>
   );
 }
