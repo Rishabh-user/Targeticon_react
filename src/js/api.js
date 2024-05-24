d84385d592ece8042a6cb2cd2a390a20019facf6
kunwaranilkumar <141806443+kunwaranilkumar@users.noreply.github.com>
2024-05-24 15:19:35 +0530

    [api endpoint]

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,9 @@
 export const apiEndpoint = "https://target-icon-cc2c549fc12a.herokuapp.com/api";
-export const apiEndpoint1 = "https://targeticon.com/api/v2/items";
-export const apiCategory = "https://targeticon.com/api/v2/categories";
-export const apicareer = "https://targeticon.com/api/v2";
+export const apiEndpoint1 = "https://securestaging.net/targeticon-api/api/v2/items";
+export const apiCategory = "https://securestaging.net/targeticon-api/api/v2/categories";
+export const apicareer = "https://securestaging.net/targeticon-api/api/v2";
+
+
+//https://securestaging.net/targeticon-api/api/v2/
+
+//https://targeticon.com/api/v2/items/read
